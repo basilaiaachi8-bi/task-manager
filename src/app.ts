5fe999d13cb4b339abacc47dbbdd726692d91d63
Achi basilaia <basilaiaachi8@gmail.com>
2026-05-05 19:48:18 +0400

    add root route

--- src/app.ts
+++ src/app.ts
@@ -14,3 +14,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
+});
